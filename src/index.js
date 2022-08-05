d6ae336c5de4a6a9e99b35177f805aad22aa8ef0
Guston-era <me@gustonera.co.ke>
2022-08-05 04:40:24 +0300

    set up graph ql

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+// graphql config
+const client = new ApolloClient({
+  uri: 'https://graphql-compose.herokuapp.com/user/',
+  cache: new InMemoryCache(),
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
 )
