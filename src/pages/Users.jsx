b0a8bdb978c1fec3b96d943fdd001c46f1c5c57c
Guston-era <me@gustonera.co.ke>
2022-08-05 04:56:28 +0300

    create first users page with gql query

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,7 +1,63 @@
+import { useQuery } from '@apollo/client'
 import React from 'react'
+import { Table } from 'react-bootstrap'
+import { GET_MANY_USERS } from '../exportedgql/queries'
 
 const Users = () => {
-  return <div>Users</div>
+  const { error, loading, data } = useQuery(GET_MANY_USERS, {
+    variables: {
+      limit: 80,
+    },
+  })
+
+  return (
+    <div>
+      {error && (
+        <div className="alert alert-danger">
+          An error occurred while fetching your data
+        </div>
+      )}
+      {loading && (
+        <div className="alert alert-info" align="center">
+          Sit back we loading you data
+        </div>
+      )}
+      {data && (
+        <>
+          <Table hover striped>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Email</th>
+                <th>Salary</th>
+                <th>Languages</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.userMany.map((u, i) => (
+                <tr key={u._id}>
+                  <td style={{ color: 'blue' }}>{u.name}</td>
+                  <td>{u.age}</td>
+                  <td>{u.gender}</td>
+                  <td>{u.contacts.email}</td>
+                  <td>{u.salaryDecimal}</td>
+                  <td>
+                    <ul>
+                      {u.languages.map((l, i) => (
+                        <li key={i}>{`${l.language} (${l.skill})`}</li>
+                      ))}
+                    </ul>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </div>
+  )
 }
 
 export default Users
