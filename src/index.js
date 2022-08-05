3ca8a971ca2987f7c457d6b690a8f7f3e9a2b38a
Guston-era <me@gustonera.co.ke>
2022-08-05 05:00:04 +0300

    include bootstrap css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // graphql config
 const client = new ApolloClient({
